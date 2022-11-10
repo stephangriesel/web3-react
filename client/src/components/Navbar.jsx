d71a65a9a33bc745aa175a29944f85c2c2f4fb5b
Stephan Griesel <sgriesel@gmail.com>
2022-11-10 08:31:01 +0100

    mobile menu finish

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <nav className='w-full flex md:justify-center justify-between items-center p-4'>
       <div className='md:flex-[0.5] flex-initial justify-center items-center'>
-        <img src={logo} alt="logo" className='w-32 cursor-pointer' />
+        <img src={logo} alt="logo" className='w-16 cursor-pointer' />
       </div>
       <ul className='text-white md:flex hidden flex-row justify-between items-center flex-initial'>
         {
@@ -35,7 +35,18 @@ const Navbar = () => {
           :
           <HiMenu fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-          // TODO: mobile menu goes here
+          <ul
+          className='z-10 fixed top-0 -right-2 p3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
+          >
+            <li className='text-xl w-full my-2'>
+              <AiOutlineCloseCircle onClick={() => setToggleMenu(false)} />
+            </li>
+            {
+              ["Market", "Exchange", "Wallets"].map((item, index) => (
+                <NavbarItem key={item + index} title={item} classProps="my-2 text-lg"/>
+              ))
+            }
+          </ul>
         )}
       </div>
     </nav>
