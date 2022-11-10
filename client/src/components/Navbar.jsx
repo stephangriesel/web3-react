42e47290c489eac28f321438e93f1c9c41d11a31
Stephan Griesel <sgriesel@gmail.com>
2022-11-10 06:46:50 +0100

    navbar set up & navitems render

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { HiMenu } from 'react-icons/hi'
+import {AiOutlineCloseCircle} from 'react-icons/ai'
+
+import logo from '../../images/logo.svg';
+
+const NavbarItem = ({title, classProps}) => {
+  return (
+    <li className={`mx-4 cursor-pointer ${classProps}`}>
+      {title}
+    </li>
+  )
+}
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+        <img src={logo} alt="logo" className='w-32 cursor-pointer' />
+      </div>
+      <ul className='text-white md:flex hidden flex-row justify-between items-center flex-initial'>
+        {
+          ["Market", "Exchange", "Wallets"].map((item, index) => (
+            <NavbarItem key={item + index} title={item} />
+          )) 
+        }
+      </ul>
+    </nav>
   )
 }
 
