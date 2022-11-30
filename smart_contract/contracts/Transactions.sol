03c218af7f24cde63ea0a02b3e2561a365630f90
Stephan Griesel <sgriesel@gmail.com>
2022-11-30 07:59:18 +0100

    push transactions

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
@@ -18,7 +18,9 @@ contract Transactions {
 
     TransferStruct[] transactions;
 
-    function addToBlockChain() public{
+    function addToBlockChain(address payable receiver, uint amount, string memory message, string memory keyword) public{
+        transactionCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
     }
     function getAllTransactions() public view returns (TransferStruct[] memory){
