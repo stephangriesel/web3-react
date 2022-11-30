74d20d041f26fdf09268466c2651d146e0eb025d
Stephan Griesel <sgriesel@gmail.com>
2022-11-30 08:09:46 +0100

    emit transfer properties

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -22,11 +22,13 @@ contract Transactions {
         transactionCount += 1;
         transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+
     }
     function getAllTransactions() public view returns (TransferStruct[] memory){
-        // return transactions
+        return transactions;
     }
     function getTransactionCount() public view returns (uint256) {
-        // return transactionCount
+        return transactionCount;
     }
 }
