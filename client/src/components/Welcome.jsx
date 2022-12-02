9ca0cf159287d22799db209ad66c9dc063a06463
Stephan Griesel <sgriesel@gmail.com>
2022-12-02 06:41:16 +0100

    test data

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { AiFillPlayCircle } from 'react-icons/ai'
 import { SiEthereum } from 'react-icons/si'
 import { BsInfoCircle} from 'react-icons/bs'
 import { BiSend } from 'react-icons/bi'
 
+import { TransactionContext } from '../context/TransactionContext'
 import Loader from './Loader'
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
@@ -20,6 +21,8 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 )
 
 const Welcome = () => {
+  const { value } = useContext(TransactionContext);
+  console.log("check value: ",value);
   const connectWallet = () => {
 
   }
