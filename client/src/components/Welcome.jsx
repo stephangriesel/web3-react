f7b512ec45ab84e655c099add7ea24e03f73a610
Stephan Griesel <sgriesel@gmail.com>
2022-12-08 07:26:32 +0100

    accept formdata context

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -21,7 +21,7 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 )
 
 const Welcome = () => {
-  const { connectWallet, currentAccount } = useContext(TransactionContext);
+  const { connectWallet, currentAccount, formData, setFormData, handleChange } = useContext(TransactionContext);
   console.log("check connect wallet: ",connectWallet);
   const handleSubmit = () => {
 
@@ -74,10 +74,10 @@ const Welcome = () => {
           </div>
 
           <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}}/>
-            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}}/>
-            <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={() => {}}/>
-            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}}/>
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange}/>
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange}/>
+            <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={handleChange}/>
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange}/>
             <div className='h-[1px] w-full bg-gray-400 my-2' />
             {false ? (
               <Loader />
